a7d923c9a9180303a487f4f1b183dd91885ba1c5
HarryYouAreAWizard <136560270+HarryYouAreAWizard@users.noreply.github.com>
2026-08-14 17:59:20 +0200

    Added polygon drawing with mouse

---
+++ polygonanalyser.py
@@ -0,0 +1,33 @@
+
+
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+
+mouse_movements_folder = Path("..") / "mouse_movements"
+
+all = "all_apparatus.txt"
+yb = "yellow_black.txt"
+
+def load_polygon_corners(file):
+    xs, ys = [], []
+    with open(mouse_movements_folder / file) as doc: 
+        for line in doc:
+            if "Button Button.left Pressed at" in line:
+                x, y = line.split("(")[1].split(")")[0].split(",")
+                xs.append(int(x))
+                ys.append(int(y))
+    xs = np.array(xs)
+    ys = np.array(ys)
+    return xs, ys
+
+xs_all, ys_all = load_polygon_corners(all)
+xs_yb, ys_yb = load_polygon_corners(yb)
+
+fig, ax=plt.subplots()
+ax.plot(xs_all - np.min(xs_all), -(ys_all - np.min(ys_all)), label="Entire apparatus")
+ax.plot(xs_yb - np.min(xs_yb), -(ys_yb - np.min(ys_yb)), label="Only yellow-black")
+ax.legend()
+fig.tight_layout()
+fig.savefig(Path("..") / "figures" / "Apparatus polygons")
+plt.close()
---
+++ recordmouse.py
@@ -0,0 +1,24 @@
+from pynput import mouse
+
+def on_move(x, y):
+    print(f"Mouse moved to position: ({x}, {y})")
+
+def on_click(x, y, button, pressed):
+    action = "Pressed" if pressed else "Released"
+    print(f"Button {button} {action} at ({x}, {y})")
+    
+    # Optional: Stop the listener by returning False when clicking right button
+    if button == mouse.Button.right and not pressed:
+        print("Stopping listener...")
+        return False
+
+def on_scroll(x, y, dx, dy):
+    direction = "Down/Right" if (dx < 0 or dy < 0) else "Up/Left"
+    print(f"Scrolled {direction} at ({x}, {y})")
+
+# This starts a background thread to listen for mouse events
+with mouse.Listener(
+        on_move=on_move, 
+        on_click=on_click, 
+        on_scroll=on_scroll) as listener:
+    listener.join()
