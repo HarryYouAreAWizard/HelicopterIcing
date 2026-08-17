8c42d711d9703c1e44bad84ab472acdf91a3ccab
HarryYouAreAWizard <136560270+HarryYouAreAWizard@users.noreply.github.com>
2026-08-17 14:03:09 +0200

    created directory handlers

--- polygonanalyser.py
+++ polygonanalyser.py
@@ -4,7 +4,14 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
-mouse_movements_folder = Path("..") / "mouse_movements"
+# data directory
+mouse_movements_dir = Path("..") / "mouse_movements"
+polygons_dir = Path("..") / "polygons"
+figures_dir = Path("..") / "figures"
+
+if not polygons_dir.is_dir():
+    polygons_dir.mkdir(parents=True, exist_ok=True)
+
 
 all = "all_apparatus.txt"
 yb = "yellow_black.txt"
@@ -12,7 +19,7 @@ yb = "yellow_black.txt"
 def load_polygon_corners(file):
     # run through the file and pick out when the left button is pressed
     xs, ys = [], []
-    with open(mouse_movements_folder / file) as doc: 
+    with open(mouse_movements_dir / file) as doc: 
         for line in doc:
             if "Button Button.left Pressed at" in line:
                 x, y = line.split("(")[1].split(")")[0].split(",")
@@ -25,10 +32,15 @@ def load_polygon_corners(file):
 xs_all, ys_all = load_polygon_corners(all)
 xs_yb, ys_yb = load_polygon_corners(yb)
 
+np.save(polygons_dir / "xs_all", xs_all)
+np.save(polygons_dir / "ys_all", ys_all)
+np.save(polygons_dir / "xs_yb", xs_yb)
+np.save(polygons_dir / "ys_yb", ys_yb)
+
 fig, ax=plt.subplots()
 ax.plot(xs_all - np.min(xs_all), -(ys_all - np.min(ys_all)), label="Entire apparatus")
 ax.plot(xs_yb - np.min(xs_yb), -(ys_yb - np.min(ys_yb)), label="Only yellow-black")
 ax.legend()
 fig.tight_layout()
-fig.savefig(Path("..") / "figures" / "Apparatus polygons")
+fig.savefig(figures_dir / "Apparatus polygons")
 plt.close()
