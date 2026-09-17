f1452879678449acbfadcbd5316d06acd1c0b15e
HarryYouAreAWizard <noah0095@gmail.com>
2026-09-17 09:54:53 +0200

    added base points in pipe frame

--- hi/convmodel.py
+++ hi/convmodel.py
@@ -61,75 +61,83 @@ class Linefinder(nn.Module):
         self.mean = None
         self.std = None
 
-        # self.conv1 = nn.Conv2d(in_channels=channels, out_channels=out_channels, kernel_size=filter_size) # 1 input channel, 3 output channels, 5x5 window
-        # self.pool = nn.functional.avg_pool2d
-
-        # dummy = ones((1, *frame_shape))
-
-        # dummy = self.conv1(dummy)
-        # dummy = self.pool(dummy, kernel_size=self.pooling_kernel_size)
-        # print(f"{dummy.shape = }")
-
-        # out_size = dummy.numel()
-
-        # self.fc1 = nn.Linear(out_size, 2)
+        # to acess training history
+        self.training_losses = []
+        self.validation_losses = []
 
     def forward(self, x):
 
         x = self.features(x)
         x = self.regressor(x)
         return x
-        # x = self.conv1(x)
-
-        # x = self.pool(x, kernel_size=self.pooling_kernel_size)
-        # # flatten for linear
-        # x = x.reshape(x.shape[0], -1)
-
-        # x = self.fc1(x)
-
-        # return x
+ 
 
     def predict(self, x):
-        # x = (x - mean) / std
+        # inference
         with torch.no_grad():
             x = self.forward(x)
+
+        # use the normalization parameters stored from training data
         x = x * self.std + self.mean
         return x
 
     def save(self):
+        """custom save function, ensuring the normalization parameters are saved along with the model"""
         torch.save(self.state_dict(), model_dir / "model_weights.pth")
         np.save(model_dir / "model_norm_params.npy", [self.mean, self.std])
+        np.save(model_dir / "training_losses.npy", self.training_losses)
+        np.save(model_dir / "validation_losses.npy", self.validation_losses)
 
-    def load(self):
-        self.load_state_dict(torch.load(model_dir / "model_weights.pth"))
-        mean, std = np.load(model_dir / "model_norm_params.npy")
-        self.mean = mean
-        self.std = std
+    def load(self, best=False):
+        """custom load function, ensuring the normalization parameters are loaded along with the model"""
+        if best:
+            self.load_state_dict(torch.load(model_dir / "model_weights_best.pth"))
+        else:
+            self.load_state_dict(torch.load(model_dir / "model_weights.pth"))
+
+        self.mean, self.std = np.load(model_dir / "model_norm_params.npy")
+        self.training_losses = list(np.load(model_dir / "training_losses.npy"))
+        self.validation_losses = list(np.load(model_dir / "validation_losses.npy"))
+
 
 class MarkedFrames(torch.utils.data.Dataset): 
+    """Custom class storing the generated points along with the corresponding video data
+    
+    TODO: ensure labels and video name are stored together
+
+    Currently only implemented for 
+    "training_labels_v2.csv" and "Video_Test_F2787_125743_01_VIDCKPT_sec.mpg"
+    """
 
     def __init__(self, video_filename, labels_path):
         super().__init__()
 
+        # load the video and scroll to start
         self.data = cv2.VideoCapture(data_dir / video_filename)
         self.data.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
-
+        
+        # load the corresponding labels. TODO: ensure labels and video are connected
         self.labels_raw = pandas.read_csv(labels_path)
+        
+        # load the mask used to extract pixels at the pipe
         self.pipe_mask = mask_creation.get_mask(self.data, "pipe_small")
 
+        # using the mask, find the rectangle in the frame to feed the model
         self.nonzero = cv2.findNonZero(self.pipe_mask).reshape((-1, 2))
         self.x_min = self.nonzero[:, 0].min()
         self.x_max = self.nonzero[:, 0].max()
         self.y_min = self.nonzero[:, 1].min()
         self.y_max = self.nonzero[:, 1].max()
 
-        # only training data with at least one (should be two) point(s) are valid
+        # there can be errors in the label set, so we have to sort out the ones with no points
+        # only training data with at least two points are valid
         self.available_indices = []
         for key in self.labels_raw.keys():
+            # this entry is always present, likely due to initialization of dataframe with no entries
             if key == "Unnamed: 0":
                 continue
 
+            # get valid points. They are either strings or nan values. We require at least two string entry
             points = self.labels_raw[key]
             valid_points = [point for point in points if isinstance(point, str)]
 
@@ -137,102 +145,143 @@ class MarkedFrames(torch.utils.data.Dataset):
             if len(valid_points) >= 2:
                 self.available_indices.append(int(key))
 
-        # for plotting the slope
-        self.relevant_xs = np.linspace(30, 50, 100)
+        # self.available indices now stores the video-indices available from the labeled dataset
+
+        # labels must be normalized
+        self.mean = None
+        self.std = None
+        self.normalize_labels() # internally overwrites the mean and std variables
 
         # to optimize data load
         self.previous_index = None
 
-        # labels must be normalized
-        self.normalize_labels()
+        # for plotting the slope
+        self.relevant_xs = np.linspace(0, 50, 100)
 
 
     def normalize_labels(self):
+        """normalize labels using the sample mean and standard deviation
+        TODO: rewrite to numpy format
+        """
+        # get slope and biases using custom loader
         slopes = []
         biases = []
         for i in range(self.__len__()):
+
+            # pick out the frame index, and use it to get the slope and bias
             idx = self.available_indices[i]
             slope, bias = self.get_slope_and_bias(idx)
 
             slopes.append(slope)
             biases.append(bias)
-            # for s, b in zip(slope, bias):
         slopes = np.array(slopes, dtype=np.float32)
         biases = np.array(biases, dtype=np.float32)
+
+        # initialize labels as slopes and biases, and immediately normalize them
+        self.labels=np.empty((slopes.shape[0], 2))
         slope_mean = np.mean(slopes)
         bias_mean = np.mean(biases)
         slope_std = np.std(slopes)
         bias_std = np.std(biases)
-
-        self.mean = np.array([slope_mean, bias_mean], dtype=np.float32)
-        self.std = np.array([slope_std, bias_std], dtype=np.float32)
-        self.labels=np.empty((slopes.shape[0], 2))
         self.labels[:, 0] = (slopes - slope_mean) / slope_std
         self.labels[:, 1] = (biases - bias_mean) / bias_std
-        # mean = np.array([-1.2883058, 85.34431], dtype=np.float32)
-        # std = np.array([0.42606908, 21.239735], dtype=np.float32)
+
+        # save the means and stds such that they can be stored in the model later
+        self.mean = np.array([slope_mean, bias_mean], dtype=np.float32)
+        self.std = np.array([slope_std, bias_std], dtype=np.float32)
 
     def __len__(self):
+        # the available length is the number of labels with valid entries
         return len(self.available_indices)
 
     def __getitem__(self, apparent_index):
+        """
+
+
+
+        Setting the frame in a cv2.VideoCapture object is a slow process, and we want to avoid it if possible
+        Using self.previous index, we can know whether the frame we read is the next one, and thereby 
+        avoid using the set function
+
+
+        """
+        # The torch dataloader uses indicies from 0 to __len___-1, we convert them to the video-indicies here 
         index = self.available_indices[apparent_index]
 
-        # Seek for the first frame or non-sequential access.
+        # check whether we must use the frame set function, and use it if nessecary
         if self.previous_index is None or index != self.previous_index + 1:
             self.data.set(cv2.CAP_PROP_POS_FRAMES, index)
 
         self.previous_index = index
 
+        # read data
         ret, frame = self.data.read()
         if not ret:
             return
-        
+
+        # use mask, and convert to grayscale
         frame = frame * self.pipe_mask[:, :, None]
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # pick out the pixels in the box surrounding the mask
         frame = gray[self.y_min:self.y_max, self.x_min:self.x_max]
 
-        frame = prep_for_torch(frame)
+        # convert to interval [0, 1]
+        frame = (frame/255).astype(np.float32)
 
-        # slope, bias = self.get_slope_and_bias(index)
-        # label = np.array((slope, bias), dtype=np.float32)
-        # label = (label - self.mean) / self.std
-        # label = tensor(label)
+        # add channel dimension
+        frame = tensor(frame).unsqueeze(0)
+
+        # pick out the normalized label
         label = self.labels[apparent_index]
 
         return frame, label
     
     def get_slope_and_bias(self, index):
+        """Get the slope and biases from an entry in the label dataset
+
+        requires index to be a video.index, not apparent index
+        
+        """
+        # pick out points from the pandas dataframe
         points_str = self.labels_raw[str(index)]
 
+        # convert points to integers in numpy arrays
         n_points = len(points_str)
         xs = []
         ys = []
         for i, point in enumerate(points_str):
-
+            # ignore the nans, hence the python list implementation
             if not isinstance(point, str) and np.isnan(point):
                 continue
+            # handle the string formatting
             x, y = point.strip("()").split(",")
             x = int(x)
             y = int(y)
             xs.append(x)
             ys.append(y)
-
         xs = np.array(xs)
         ys = np.array(ys)
+
+        # use numpy linear regression to get slope and bias (intercept)
+        # try-except structure is for debugging
         try:
             slope, bias = np.polyfit(xs, ys, 1)
         except TypeError:
             print(index)
             print(f"{self.labels[str(index)] = }")
             raise TypeError
+        
+        # convert to flost32 to ensure compatibility with torch
         slope = slope.astype(np.float32)
         bias = bias.astype(np.float32)
 
         return slope, bias
 
     def get_points(self, apparent_index):
+        """
+        not used internally, but provides access to the raw points in the label dataset
+        """
         index = self.available_indices[apparent_index]
         points_str = self.labels_raw[str(index)]
 
@@ -320,17 +369,19 @@ def load_model(load_weights=True):
     print(f"Trainable parameters: {trainable_params:,}")
 
     if load_weights:
-        model.load()
+
+        model.load(best=True) if load_weights=="best" else model.load()
         print(f"{model.mean = }")
         print(f"{model.std = }")
 
     return model, loss_func, optimizer, scheduler
 
 
-
-
-def load_training_data(batch_size):
+def load_data_with_labels()->MarkedFrames:
     data = MarkedFrames(video_filename=video_filename, labels_path=label_dir / "training_labels_v2.csv")
+    return data
+
+def prepare_and_split_data(data, batch_size)->tuple:
 
     train_size = int(0.70 * len(data))
     val_size = int(0.15 * len(data))
@@ -342,7 +393,6 @@ def load_training_data(batch_size):
         generator=torch.Generator().manual_seed(42)
     )
 
-    
     train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
     val_dataloader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
     test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
@@ -353,10 +403,8 @@ def load_training_data(batch_size):
         test_data,
         train_dataloader,
         val_dataloader,
-        test_dataloader,
-        data,
+        test_dataloader
     )
-
 
 def plot_test(model, data, test_idx):
     model.eval()
@@ -385,43 +433,33 @@ def plot_test(model, data, test_idx):
         print(f"{point = }")
         cv2.drawMarker(frame_drawable, point, 200, 1, markerSize=5)
 
-    cv2.imwrite("figures\\image.png", frame_drawable)
+    cv2.imwrite(figure_dir / "image.png", frame_drawable)
 
 
-def train(do_train=True, load_weights=True, epochs=2):
+def train(model_entries, data, batch_size=10, epochs=2):
+    # unpack the model entries
+    model, loss_func, optimizer, scheduler = model_entries
 
-    batch_size = 10
-    model, loss_func, optimizer, scheduler = load_model(load_weights=load_weights)
+    # prepare data for training
     (
         train_data,
         val_data,
         test_data,
         train_dataloader,
         val_dataloader,
-        test_dataloader,
-        data
-    ) = load_training_data(batch_size=batch_size)
+        test_dataloader
+    ) = prepare_and_split_data(data, batch_size=batch_size)
 
+    # store the normalization parameters in the model
     if model.mean is None and model.std is None:
         # training set mean and std are known, we give them directly to the model
         model.mean = data.mean
         model.std = data.std
 
-    if load_weights:
-        losses = np.load(model_dir / "losses.npy")
-        losses = list(losses)
-        validation_losses = np.load(model_dir / "validation_losses.npy")
-        validation_losses = list(validation_losses)
-
-    else:
-        losses = []
-        validation_losses = []
 
     model.train()
     for epoch in range(epochs):
 
-        if not do_train: 
-            break
 
         model.train()
         # ------------training sequence------------
@@ -432,7 +470,7 @@ def train(do_train=True, load_weights=True, epochs=2):
             loss = loss_func(predictions, labels)
             loss.backward()
             optimizer.step()
-            losses.append(loss.detach().numpy())
+            model.training_losses.append(loss.detach().numpy())
 
         # ------------validation sequence------------
         model.eval()
@@ -441,24 +479,21 @@ def train(do_train=True, load_weights=True, epochs=2):
                 frames, labels = batch
                 predictions = model(frames)
                 validation_loss = loss = loss_func(predictions, labels)
-                validation_losses.append(validation_loss.detach().numpy())
+                model.validation_losses.append(validation_loss.detach().numpy())
                 scheduler.step(validation_loss)
 
-                if validation_loss == np.min(validation_losses):
-                    # torch.save(model.state_dict(), "model_weights_best.pth")
-                    model.save()
-        # ------------end of epoch housekeeping------------
-        # torch.save(model.state_dict(), "model_weights.pth")
-        model.save()
-        np.save(model_dir / "losses.npy", losses)
-        np.save(model_dir / "validation_losses.npy", validation_losses)
+                # in the case where the best validation errors are archived, we save the model parameters
+                if validation_loss == np.min(model.validation_losses):
+                    torch.save(model.state_dict(), model_dir / "model_weights_best.pth")
 
+        # ------------end of epoch housekeeping------------
+        model.save()
 
         fig, axs=plt.subplots(1, 2)
-        axs[0].plot(losses)
-        axs[1].plot(validation_losses)
-        axs[0].set_title(f"losses")
-        axs[1].set_title(f"validation_losses")
+        axs[0].plot(model.training_losses)
+        axs[1].plot(model.validation_losses)
+        axs[0].set_title(f"Training losses")
+        axs[1].set_title(f"Validation losses")
         fig.savefig(figure_dir / "losses.png")
         plt.close()
 
@@ -466,52 +501,99 @@ def train(do_train=True, load_weights=True, epochs=2):
 
 
     # ------------end of traning houesekeeping------------
-    losses = np.array(losses)
-    validation_losses = np.array(validation_losses)
-    np.save(model_dir / "losses.npy", losses)
-    np.save(model_dir / "validation_losses.npy", validation_losses)
     model.save()
-    # torch.save(model.state_dict(), "model_weights.pth")
+
     fig, axs=plt.subplots(1, 2)
-    axs[0].plot(losses)
-    axs[1].plot(validation_losses)
+    axs[0].plot(model.training_losses)
+    axs[1].plot(model.validation_losses)
+    axs[0].set_title(f"Training losses")
+    axs[1].set_title(f"Validation losses")
     fig.savefig(figure_dir / "losses.png")
     plt.close()
 
-    # ------------examples of model inference------------
-    for i in range(0, len(data), 5):
-        plot_test(model, data, i)
-        sleep(1)
 
+def calculate_base_slope_bias(data):
+    frame, frame_raw = data[0]
 
+    frame = frame[0, :, :].numpy()
+    # frame.shape = [1 48, 52] = [C, H, W]
+    xmin = 0
+    ymin = 0
+    xmax = frame.shape[1] - 1
+    ymax = frame.shape[0] - 1
 
-def livestream_test(start):
+    list_of_xs_ys_where_nonzero = cv2.findNonZero(frame)
+
+    x_left,   y_left    = list_of_xs_ys_where_nonzero[list_of_xs_ys_where_nonzero[:, 0] == xmin][-1]
+    x_top,    y_top     = list_of_xs_ys_where_nonzero[list_of_xs_ys_where_nonzero[:, 1] == ymin][-1]
+    x_right,  y_right   = list_of_xs_ys_where_nonzero[list_of_xs_ys_where_nonzero[:, 0] == xmax][-1]
+    x_bottom, y_bottom  = list_of_xs_ys_where_nonzero[list_of_xs_ys_where_nonzero[:, 1] == ymax][-1]
+    # print(f"left: {x_left, y_left}")
+    # print(f"top: {x_top, y_top}")
+    # print(f"right: {x_right, y_right}")
+    # print(f"bottom: {x_bottom, y_bottom}")
+
+    base_slope = (y_right - y_bottom) / (x_right - x_bottom)
+    base_bias = (y_right - base_slope * x_right)
+
+    return (
+        base_slope, 
+        base_bias,
+        (
+            (x_left,   y_left),
+            (x_top,    y_top),
+            (x_right,  y_right),
+            (x_bottom, y_bottom),
+        )
+    )
+
+def livestream_test(model, start):
 
     data = PureVideo(video_filename)
-    model,_,_,_ = load_model(load_weights=True)
+
     model.eval()
     show_gray = False
+
+    base_slope, base_bias, corners = calculate_base_slope_bias(data)
 
     xs = np.linspace(0, 50, 100)
     i = start
     while data.data.isOpened():
         frame, frame_raw = data[i]
+
         frame = frame.unsqueeze(0) # add batch dimension
 
         frame_drawable = unprep_from_torch(frame).squeeze(0)
 
         with torch.no_grad():
             predictions = model.predict(frame)
+
         slope, bias = predictions[0].detach().numpy()
         ys = slope * xs + bias
         point1 = (np.uint8(xs[0]),  np.uint8(ys[0]))
         point2 = (np.uint8(xs[-1]), np.uint8(ys[-1]))
-        cv2.line(frame_drawable, point1, point2, 10000, 1)
+        # cv2.line(frame_drawable, point1, point2, 10000, 1)
         if show_gray:
             frame_raw = cv2.cvtColor(frame_raw, cv2.COLOR_BGR2GRAY)
+
+        ys = base_slope * xs + base_bias
+        point1 = (np.uint8(xs[0]),  np.uint8(ys[0]))
+        point2 = (np.uint8(xs[-1]), np.uint8(ys[-1]))
+        cv2.line(frame_drawable, point1, point2, 10000, 1)
+
+
         cv2.imshow("Original", frame_raw)
         cv2.imshow("Pipe", frame_drawable)
-        # cv2.imwrite("Pipe.png", frame_drawable)
+
+        print(f""
+              + f"slope:       {slope:.3}, "
+              + f"bias:        {bias:.3}, "
+              + f"base_slope:  {base_slope:.3}, "
+              + f"base_bias:   {base_bias:.3}, "
+              , end="\r")
+
+
+
         i += 1
         key = cv2.waitKey(1)
         if key == ord("q"):
--- hi/main.py
+++ hi/main.py
@@ -6,18 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import os
+import torch
 
 from pathlib import Path
 
-from video import filter_frame, change_color_representation, extract_pixels_in_mask, normalize_brightness, set_frame
-from mask_creation import reduced_frame_polygon, create_mask
-
+import convmodel
 import mask_creation
-import video
-from ice_noice import get_video_frame_averaged_HSV, get_video_frame_FULL_HSV, silhouette_score_from_mean_HSVs 
 import ice_noice
-from pynput import mouse
-
 figure_dir          = Path(os.getcwd()) / "figures"
 data_dir            = Path(os.getcwd()) / "video-data"
 polygon_dir         = Path(os.getcwd()) / "polygons"
@@ -25,6 +20,8 @@ case_videos_dir     = Path(os.getcwd()) / "case_videos"
 segnet_output_dir   = Path(os.getcwd()) / "segnet_output"
 scan_output_dir     = Path(os.getcwd()) / "scan_output"
 # reduced_video_dir   = Path(os.getcwd()) / "reduced_video_data"
+label_data_dir      = Path(os.getcwd()) / "label_data"
+model_weights_dir   = Path(os.getcwd()) / "model_weights"
 
 
 video_filename = "Video_Test_F2787_125743_01_VIDCKPT_sec.mpg"
@@ -35,9 +32,25 @@ video_filename = "Video_Test_F2787_125743_01_VIDCKPT_sec.mpg"
 
 def main()->None:
 
-    from convmodel import livestream_test
+    print(f"loading model...")
+    model_entries = convmodel.load_model(load_weights=False)
+    model, loss_func, optimizer, scheduler = model_entries
+    model.mean = np.float32(0.0)
+    model.std = np.float32(1.0)
 
-    livestream_test(0)
+    convmodel.livestream_test(model, 0)
+    return
 
+    print(f"loading data...")
+    data = convmodel.load_data_with_labels()
+
+
+    print(f"starting training...")
+    convmodel.train(
+        model_entries=model_entries,
+        data=data,
+        batch_size=20,
+        epochs=25
+    )
 
 main()
